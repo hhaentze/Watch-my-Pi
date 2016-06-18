9b41b3dd15076b014521b922833d0aceadfd4d1f
hackopedia31 <wladi.schneider13@googlemail.com>
2016-06-18 21:46:40 +0200

    dem Ende mega nah

--- cpu_load.py
+++ cpu_load.py
@@ -4,13 +4,13 @@ import Memory
 import send_email
 #import email
 #config values
-
+while_loop = True
 while(True):
 
 	cpu_load_warning = 10 #70   #value for cpu warning message
 	cpu_interval = 3      #intval for cpu_percant_reequest
 	cpu_loadinglevel = False
-
+	cpu_load_finish = 0
 	def cpu_load():
 		count = psutil.cpu_count()
 		condition_cpu_loop = True
@@ -20,6 +20,7 @@ while(True):
 		while (condition_cpu_loop):
 			cpu_load = psutil.cpu_percent(interval=cpu_interval)
 			print(cpu_load)
+			cpu_load_finish = cpu_load
 			if(cpu_load > cpu_load_warning):
 				condition_cpu_loop = False
 				print("Warning Warning")
@@ -33,10 +34,16 @@ while(True):
 
 
 	print(cpu_load())
-	if (cpu_load >= 10) & (cpu_loadinglevel == False):
+	if (cpu_load_finish >= 80) & (cpu_loadinglevel == False):
+		#print(cpu_load + " foo")		
 		send_email.main("DANGER DANGER! Ihr Pi ist zu 85% ausgelastet - sofern sie es nicht willentlich tun, untersuchen sie ihn auf Schadsoftware.")
-		ccpu_loadinglevel = True
-	if (cpu_load >= 15) & (cpu_loadinglevel == True):
+		cpu_loadinglevel = True
+	if (cpu_load_finish >= 95) & (cpu_loadinglevel == True):
+		#print(cpu_load + " foo")
 		send_email.main("DANGER DANGER DANGER! Ihr Pi ist bereits zu 95% ausgelastet, er wird sich bald abschalten! Sichern sie ihre Daten, sonst koennte es 			zu Datenverlusten kommen!") 
 		cpu_loadinglevel = False
+		while_loop = False
+		break
+
+Print("Programm wird beendet...")
 	
