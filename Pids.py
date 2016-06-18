7dae31ad798db541460c5524ed95e77115f15b88
hackopedia31 <wladi.schneider13@googlemail.com>
2016-06-18 15:23:46 +0200

    Durchlauf fertig

--- Pids.py
+++ Pids.py
@@ -3,11 +3,12 @@ import sys
 
 Process_int = 0
 Processes = []
-
+Processes_D = {}
 pids = psutil.pids()
-#print (pids)
-#Processes.append(pids)
-#print(Processes)
+draw = 0
+PID = "PID"
+CPU = "CPU"
+PID_name = "PID_name"
 
 for pid in pids:
 	Process_int = Process_int + 1
@@ -18,19 +19,24 @@ for pid in pids:
 		pid_name = p.name()
 		cpu = p.cpu_percent(interval=1.0) 
 		if cpu > 8:
+	
 			print pid, " ", cpu," " , pid_name
 			Processes.append(pid)
+			PID = "PID"+str(draw)
+			CPU = "CPU"+str(draw)
+			PID_name = "PID_name"+str(draw)
+			PR1 = {PID:pid,CPU:cpu,PID_name:pid_name}
+			Processes_D.update(PR1)
+			draw = draw + 1
+
 		else:
 			sys.stderr.write("\x1b[2J\x1b[H")
 	else:
 		sys.stderr.write("\x1b[2J\x1b[H")
 print "total processes: ", Process_int
-for process in Processes:
+for i in range(0,draw):
 	try:
-		p = psutil.Process(int(pid))
-		pid_name = p.name()
-		cpu = p.cpu_percent(interval=1.0)
-		print pid, " ", cpu, " ", pid_name
+		print Processes_D["PID"+str(i)], " ", Processes_D["CPU"+str(i)], " ", Processes_D["PID_name"+str(i)]
 	except:
 		print "process was not found anymore"
 
