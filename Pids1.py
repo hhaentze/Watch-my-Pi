62bcb0a3fb4577b2411a2dd671b2955ac5e3e413
udv <tobias.hanf@hotmail.de>
2016-06-18 13:41:27 +0200

    Pids Update

--- Pids1.py
+++ Pids1.py
@@ -8,7 +8,9 @@ print (pids)
 #print(Processes)
 
 for pid in pids:
-	p = psutil.Process(int(pid))
+	p = psutil.Process(pid)
 	pid_name = p.name()
-	cpu = p.cpu_percent(interval=1.0)
-	print pid, " ", cpu," " , pid_name
+	cpu = p.cpu_percent(interval=0.1)
+	cpu = cpu / psutil.cpu_count()
+        if(cpu > 10):
+            print pid, "", cpu ,"", pid_name 
